4bc6bd152a6fd88fb1dce70f669789284b4f8536
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-10-19 14:56:47 +0530

    Draw the current Stroke

--- src/utils/canvasUtils.ts
+++ src/utils/canvasUtils.ts
@@ -1,3 +1,23 @@
+import { Point } from "./types";
+
+export const drawStroke = (
+  context: CanvasRenderingContext2D,
+  points: Point[],
+  color: string
+) => {
+  if (!points.length) {
+    return;
+  }
+  context.strokeStyle = color;
+  context.beginPath();
+  context.moveTo(points[0].x, points[0].y);
+  points.forEach((point) => {
+    context.lineTo(point.x, point.y);
+    context.stroke();
+  });
+  context.closePath();
+};
+
 export const clearCanvas = (canvas: HTMLCanvasElement) => {
   const context = canvas.getContext("2d");
   if (!context) {
