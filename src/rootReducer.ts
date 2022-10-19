bd25438fcb8566b4e7078a68a2e13ed70328f328
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-10-19 10:34:25 +0530

    Setting up Redux Logger

---
+++ src/rootReducer.ts
@@ -0,0 +1,9 @@
+type RootState = {};
+
+type Action = {
+  type: string;
+};
+
+export const rootReducer = (state: RootState = {}, action: Action) => {
+  return state;
+};
---
+++ src/store.ts
@@ -0,0 +1,14 @@
+import { rootReducer } from "./rootReducer";
+import {
+  composeWithDevTools,
+  devToolsEnhancer,
+} from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
+
+store.dispatch({ type: "TEST_ACTION" });
