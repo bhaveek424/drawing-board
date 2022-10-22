697a196d0f3031f36b74a4a3909724987e3c98a0
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-10-22 18:29:23 +0530

    Creating File Panel

---
+++ src/shared/FilePanel.tsx
@@ -0,0 +1,30 @@
+import { saveAs } from "file-saver";
+import { useCanvas } from "../CanvasContext";
+import { getCanvasImage } from "../utils/canvasUtils";
+
+export const FilePanel = () => {
+  const canvasRef = useCanvas();
+
+  const exportToFile = async () => {
+    const file = await getCanvasImage(canvasRef.current);
+    if (!file) {
+      return;
+    }
+    saveAs(file, "drawing.png");
+  };
+
+  return (
+    <div className="window file">
+      <div className="title-bar">
+        <div className="title-bar-text">File</div>
+      </div>
+      <div className="window-body">
+        <div className="field-row">
+          <button className="save-button" onClick={exportToFile}>
+            Export
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
