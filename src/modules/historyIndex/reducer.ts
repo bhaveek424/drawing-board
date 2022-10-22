dff79ed7760be2942cc141feed063696dfa318e4
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-10-23 01:08:36 +0530

    createReducer: updating historyIndex reducer

--- src/modules/historyIndex/reducer.ts
+++ src/modules/historyIndex/reducer.ts
@@ -1,24 +1,20 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { RootState } from "../../utils/types";
 import { endStroke } from "../sharedActions";
 import { Action, redo, undo } from "./actions";
 
-export const reducer = (
-  state: RootState["historyIndex"] = 0,
-  action: Action
-) => {
-  switch (action.type) {
-    case endStroke.toString(): {
-      return 0;
-    }
-    case undo.toString(): {
-      return Math.min(state + 1, action.payload);
-    }
-    case redo.toString(): {
-      return Math.max(state - 1, 0);
-    }
-    default:
-      return state;
-  }
-};
+const initialState: RootState["historyIndex"] = 0;
+
+export const reducer = createReducer(initialState, (builder) => {
+  builder.addCase(undo, (state, action) => {
+    return Math.min(state + 1, action.payload);
+  });
+  builder.addCase(redo, (state) => {
+    return Math.max(state - 1, 0);
+  });
+  builder.addCase(endStroke, () => {
+    return 0;
+  });
+});
 
 export const historyIndexSelector = (state: RootState) => state.historyIndex;
